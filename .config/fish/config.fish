0fe3091a3ba09c656639dc3208ae6210184ae344
Riley Sutton <riley.d.sutton@gmail.com>
2018-06-01 21:29:01 +1000

    Updated files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
+set fish_greeting ""
+set VISUAL vim
+set PSPDEV /home/riley/Documents/unsw/comp/etc/pspdev
+set PATH $PSPDEV/bin $PATH
+
 neofetch
 #echo "--------------------------------------------------------------------------------"
 #curl wttr.in/sydney\?0
 echo "--------------------------------------------------------------------------------"
+echo "file being run"
 fortune
