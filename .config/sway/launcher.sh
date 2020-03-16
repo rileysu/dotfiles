362947c3bf6011f5f86cbbf062640a58ee5d6739
Riley Sutton <riley.d.sutton@gmail.com>
2020-03-16 15:12:17 +1100

    update launcher.sh so bash is no longer a dependency

--- .config/sway/launcher.sh
+++ .config/sway/launcher.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-bash -c 'compgen -c' | sort -u | fzf --layout=reverse | xargs -r swaymsg -t command exec
+files=""
+
+for val in $(echo $PATH | tr ':' '\n'); do
+	
+	files="$files
+$(find $val -mindepth 1 -maxdepth 1 -executable -printf %f\\n)"
+
+done
+
+files=$(echo "$files" | sort | uniq) 
+
+echo "$files" | sort -u | fzf --layout=reverse | xargs -r swaymsg -t command exec
